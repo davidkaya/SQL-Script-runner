7c6bab5af7b764ba7c5c7f1b815cc3aaefbe454d
davidkaya <mail@davidkaya.sk>
2014-03-23 14:53:15 +0100

    Update SqlScriptRunner.java

--- SqlScriptRunner.java
+++ SqlScriptRunner.java
@@ -9,14 +9,10 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 /**
  *
- * @author davidkaya
+ * @author David Kaya
+ * @version 0.1
  */
 public class SqlScriptRunner {
 
